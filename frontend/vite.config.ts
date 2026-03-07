416052ec744972112540d7bcc5147f681967a4c7
root <nilesh.choubisa@dexpertsystems.com>
2026-03-07 13:36:35 +0000

    dep bug fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
-    historyApiFallback: true, // ✅ THIS IS THE FIX
+    port: 8089,
+    allowedHosts: ["lead-ai.dexpertsystems.com"],
+    historyApiFallback: true,
     proxy: {
       '/api': {
         target: 'https://dexchatbackend.dexpertsystems.com/',
